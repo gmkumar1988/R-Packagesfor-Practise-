4f27ffc307d36f927604566827577dc1dd8661b4
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2021-07-14 10:49:29 +0530

    gganimate_sample
    gganimate_sample

---
+++ gganimate.R
@@ -0,0 +1,10 @@
+install.packages("datasauRus")
+library(datasauRus)
+library(ggplot2)
+library(gganimate)
+
+ggplot(datasaurus_dozen, aes(x=x, y=y))+
+  geom_point()+
+  theme_minimal() +
+  transition_states(dataset, 3, 1) + 
+  ease_aes('cubic-in-out')
