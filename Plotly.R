42440b60c6cfb2c1006741c8de0946a1ec000bf6
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-05-26 19:23:37 -0400

    Plotly Package Practise1

---
+++ Plotly.R
@@ -0,0 +1,29 @@
+install.packages("plotly")
+library(plotly)
+library(ggplot2)
+install.packages("tidyverse")
+install.packages("dplyr")
+
+library(tidyverse)
+library(dplyr)
+data("EuStockMarkets")
+
+stocks <- as.data.frame(EuStockMarkets) %>%
+  gather(index, price) %>%
+  mutate(time = rep(time(EuStockMarkets),4))
+
+plot_ly(stocks, x = time, y = price, color = index)
+
+plot_ly(stocks, x = time, y = price)
+
+remove.packages("rlang")
+install.packages("rlang")
+update.packages("rlang")
+
+### Sample2 : 
+data("airmiles")
+library(plotly)
+plot_ly(x = time(airmiles), y = airmiles)
+
+
+# plotly(stocks)
