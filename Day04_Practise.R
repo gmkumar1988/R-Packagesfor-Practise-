9c5a8110104b2dc7738e1b7212e68319169962a6
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-07-09 19:02:04 -0400

    Daily practise

--- Day04_Practise.R
+++ Day04_Practise.R
@@ -13,4 +13,27 @@ View(M)
 diag(M) <- 0 
 which(M > 0.8, arr.ind = T)
 names(spam) [c(34,32)]
-plot(spam[,34], spam[,32])
+plot(spam[,34], spam[,32], xlab = "Spam of 34", ylab = "Spam of 32")
+
+###Predicting with Regression###
+#Fit Simple regression model 
+library(caret)
+data("faithful")
+set.seed(333)
+intrain <- createDataPartition(y= faithful$waiting,
+                                  p = 0.5, list = FALSE)
+
+# rm(trainfaith)
+trainfaith <- faithful[intrain,]
+testfaith <- faithful[-intrain,]
+
+head(trainfaith)
+plot(trainfaith$waiting, trainfaith$eruptions, pch = 19, col="blue",
+     xlab = "Waiting", ylab = "Eruptions")
+
+lml <- lm(trainfaith$eruptions ~ trainfaith$waiting)
+summary(lml)
+
+plot(trainfaith$waiting, trainfaith$eruptions, pch = 19, col="blue",
+     xlab = "Waiting", ylab = "Eruptions")
+lines(trainfaith$waiting, lml$fitted.values, lwd = 3)
