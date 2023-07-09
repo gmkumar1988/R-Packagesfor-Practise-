0a86a3eeb6ced5ee313a18e3741ee816527ec469
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-07-09 12:10:51 -0400

    Daily Practise

--- Day03_Practise.R
+++ Day03_Practise.R
@@ -2,3 +2,19 @@ library(kernlab)
 data("spam")
 spam$capitalAvesq <- spam$capitalAve^2
 head(spam$capitalAvesq)
+
+library(ISLR)
+library(caret)
+data(Wages)
+Intrain <- createDataPartition(y = Wage$wage,
+                               p = 0.7, list = FALSE)
+training <- Wage[Intrain,]
+testing <- Wage[-Intrain,]
+
+View(training)
+
+table(training$jobclass)
+
+dummies <- dummyVars(wage ~ jobclass, data = training )
+head(predict(dummies, newdata = training ))
+
