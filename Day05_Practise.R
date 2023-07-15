301cb2b43a15e980df1cc1c72b1b05121732f866
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-07-15 09:00:42 -0400

    Day05 Random Forests

--- Day05_Practise.R
+++ Day05_Practise.R
@@ -16,3 +16,51 @@ dim(testing)
 
 qplot(training$Petal.Width, training$Sepal.Width, colour = training$Species, data = training,
       xlab = "Petal Width", ylab = "Sepal Width")
+
+library(caret)
+library(rpart)
+modfit <- train(Species~ ., method = "rpart", 
+                data = training)
+print(modfit$finalModel)
+
+plot(modfit$finalModel, uniform = TRUE, 
+     main = "Classification Tree")
+text(modfit$finalModel, use.n = TRUE, all = TRUE, cex = 0.8)
+
+
+install.packages("rattle")
+library(rattle)
+fancyRpartPlot(modfit$finalModel)
+
+predict(modfit, newdata = testing)
+
+# install.packages("ElemStatLearn")
+# library(ElemStatLearn)
+
+
+###Random Forests ###
+data(iris)
+library(ggplot2)
+intrain <- createDataPartition(y = iris$Species, 
+                               p = 0.7, list = FALSE)
+rm(training)
+training <- iris[intrain,]
+testing <- iris[-intrain,]
+
+library(caret)
+install.packages("randomForest")
+library(randomForest)
+modfit <- train(Species ~ ., data = training, method = "rf", prox = TRUE)
+modfit
+
+getTree(modfit$finalModel, k = 2)
+
+irisP <- classCenter(training[, c(3,4)], training$Species, modfit$finalModel$proximity)
+irisP <- as.data.frame(irisP)
+irisP$Species <- rownames(irisP)
+
+View(irisP)
+p <- qplot(training$Petal.Width, training$Petal.Length, colour = training$Species,
+           data = training)
+p + geom_point(aes(x = Petal.Width, y = Petal.Length, col = Species
+                   ), size = 5, shape = 4, data = irisP)
