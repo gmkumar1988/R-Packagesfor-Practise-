18366e8c5dd0a2ccbd1512ec9122ead93a52085c
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-09-16 10:32:22 -0400

    PredictionMLCoursera

--- Day11_Practise.R
+++ Day11_Practise.R
@@ -2,3 +2,11 @@ install.packages("wikifacts")
 install.packages("ggplot2")
 library(wikifacts)
 library(ggplot2)
+install.packages("shinyapps")
+update.packages("shinyapps")
+
+install.packages("rsconnect")
+library(rsconnect)
+rsconnect::setAccountInfo(name='gmkumar1988',
+                          token='A1D88FFDAD4E818F85890EC6F7829F0B',
+                          secret='LwhOA8NcRa+MCBmSlObS19hS79f5t1mhzOffnoQ3')
