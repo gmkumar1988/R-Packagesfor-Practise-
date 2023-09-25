4e225eab79760ab8942df0d46918b59580093bba
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-09-25 19:44:06 -0400

    Day14

---
+++ Day14.R
@@ -0,0 +1,15 @@
+library(shiny)
+
+#Define user interface 
+
+ui <- fluidPage(
+  titlePanel("My Simple App"),
+  textInput(inputId = "my_text", label = "Enter some text"),
+  textOutput(outputId = "print_text")
+)
+
+server <- function(input,output){
+  output$print_text <- renderText(input$my_text)
+}
+
+shinyApp(ui = ui, server = server)
