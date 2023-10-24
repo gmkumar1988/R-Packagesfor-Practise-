dbb4fc4f90c0ee00ec71efd298eb16dabd1ae9c3
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-10-23 21:27:27 -0400

    Daily Practise

--- Modeling_Unemployment_Rate_Timeseries.R
+++ Modeling_Unemployment_Rate_Timeseries.R
@@ -3,6 +3,7 @@ library(forecast)
 # install.packages("dygraphs")
 library(dygraphs)
 laborforce = na.omit(laborforce)
+# is.na(laborforce)
 View(laborforce)
 rownames(laborforce)
 laborforce <- laborforce[,-c(1,2)]
@@ -11,7 +12,8 @@ laborforce <- t(laborforce)
 colnames(laborforce) <- c('Year','Emp Rate')
 dim(laborforce)
 str(laborforce)
-plot(laborforce)
+plot(df, xlab = "Year", ylab = "Employment Rate", 
+     main = "Spain Labor Force Employment Rate Till 2020")
 plot.ts(laborforce)
 # holttrend =holt(laborforce, h = 5)
 
