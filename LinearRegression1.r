68e4be355e8909c5686a131e50a3426c45e85eef
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2024-01-13 12:33:32 -0500

    To randomly sample from a normal distribution, check out the rnorm()

--- LinearRegression1.r
+++ LinearRegression1.r
@@ -22,3 +22,62 @@ model <- lm(y ~ x, data = df)
 print(model)
 model <- lm(y ~ x, data = df)
 print(model)
+
+
+x = c(seq(1:10))
+x
+error_comp = rnorm(10,0,1)
+error_comp
+y=3+2*x+error_comp
+y
+qplot(x,y, main = "3 + 2*x + random error with SD = 1", ylim=c(-1, 30))
+x = c(seq(1:10))
+x
+error_comp = rnorm(10,0,2)
+error_comp
+y=3+2*x+error_comp
+y
+qplot(x,y, main = "3 + 2*x + random error with SD = 2", ylim=c(-1, 30))
+x = c(seq(1:10))
+x
+error_comp = rnorm(10,0,5)
+error_comp
+y=3+2*x+error_comp
+y
+qplot(x,y, main = "3 + 2*x + random error with SD = 5", ylim=c(-1, 30))
+
+
+x = c(seq(1:10))
+y = 3 + 2*x
+error_comp = rnorm(10,0,1)
+error_comp
+y1=3+2*x+error_comp 
+error_comp = rnorm(10,0,2)
+error_comp
+y2=3+2*x+error_comp
+error_comp = rnorm(10,0,5)
+error_comp
+y5=3+2*x+error_comp
+DATAFRAME <- data.frame(
+x = x,
+y0= y,
+y1 = y1,
+y2 = y2,
+y3 = y5
+)
+#transpose to long format except year variable
+df2 <- pivot_longer(DATAFRAME, cols = paste0("y",0:3))
+df2
+ggplot(
+data = df2,
+mapping = aes(x = x, y = value, color = name)
+) +
+geom_point(aes(size = name)) +
+geom_smooth(method = lm) +
+labs(
+title = "Abline & Dot Plot with biggest for sd of 5 thru smallest sd = 0",
+y = "y values",
+x= "x values",
+lintype = "Legend"
+) +
+theme_bw()
