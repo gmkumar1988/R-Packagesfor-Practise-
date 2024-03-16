7d12c9272e13b16b2cb3427f1f943a22bdfe3f58
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2024-03-15 23:17:22 -0400

    Daily Practise Regression

--- Module_1_Regression_Analysis.R
+++ Module_1_Regression_Analysis.R
@@ -11,3 +11,17 @@ g = ggplot(turtle_data, aes(x = turtle_data$age, y = turtle_data$income)) +
   geom_point() +
   labs(title = "Scattter Plot of Turtle Data = Income Vs Age")
 plot(g)
+
+summary(turtle_data)
+turtle_data_age_centered <- 43.00
+turtle_data_income_centered <- 60750
+df_turtle_data_centered = data.frame(age = turtle_data_age_centered,
+                                        income = turtle_data_income_centered)
+head(df_turtle_data_centered)
+g1 = ggplot(df_turtle_data_centered, aes(x = df_turtle_data_centered$age,
+                                         y = df_turtle_data_centered$income)) +
+  geom_point() +
+  labs(title = "Latest Scatter Plot of Turtle Data with Income and Age ")
+g1
+
+class(df_turtle_data_centered)
