97c469d1a50cb6c0e394605b4645b47693a83a7a
MaheshG <48827109+gmkumar1988@users.noreply.github.com>
2024-07-13 08:24:33 -0400

    JohnHopkins

---
+++ SQLQuery_2.sql
@@ -0,0 +1,16 @@
+SELECT TOP (1000) [FactID]
+      ,[Date]
+      ,[Open]
+      ,[High]
+      ,[Low]
+      ,[Close]
+      ,[AdjClose]
+      ,[Volume]
+      ,[SecurityID]
+  FROM [dbo].[FactPrices_Daily]
+
+
+  select top(1000)
+        max([DateTime]) as MaxDate,
+        min([DateTime]) as MinDate
+        from [dbo].[FactAttributes_Intraday];
