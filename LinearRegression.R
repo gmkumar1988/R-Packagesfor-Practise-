12eec3a5bf7d717a39f04a9ec558016b5d06522e
MaheshG <48827109+gmkumar1988@users.noreply.github.com>
2024-09-08 00:39:40 -0400

    Linear Regression Car Price Value

--- LinearRegression.R
+++ LinearRegression.R
@@ -23,4 +23,47 @@ View(carprice_data)
 summary(carprice_data)
 str(carprice_data)
 
-plot(carprice_data$Price ~ carprice_data$Prod..year, data = carprice_data)
+
+filtered_data <- carprice_data %>%
+  filter(carprice_data$Prod..year >=2000) %>% 
+  select(carprice_data$Price, carprice_data$Prod..year)
+
+View (filtered_data)
+head(filtered_data)
+
+plot(filtered_data$Price ~ filtered_data$Prod..year, data = filtered_data)
+options(scipen = 9999)
+hist(filtered_data$Prod..year)
+
+cor(filtered_data$Engine.volume, filtered_data$Mileage)
+as.numeric(filtered_data$Engine.volume)
+is.na(filtered_data$Engine.volume)
+plot(filtered_data$Engine.volume ~ filtered_data$Mileage, data = filtered_data)
+
+filtered_data$Engine.volume <- as.numeric(filtered_data$Engine.volume)
+suppressWarnings(as.numeric(filtered_data$Engine.volume))
+filtered_data$Engine.volume <- na.omit(filtered_data$Engine.volume)
+print(filtered_data$Engine.volume)
+
+###Replace NA with 0 in all columns: 
+filtered_data$Engine.volume[is.na(filtered_data$Engine.volume)] <- 0
+print(filtered_data$Engine.volume)
+filtered_data$Mileage[is.na(filtered_data$Mileage)] <- 0
+print(filtered_data$Mileage)
+
+##Visualize the Engine Value and Mileage of cars: 
+ggplot(data = filtered_data, mapping =aes(x = Mileage,
+                                          y = Engine.volume)) + 
+  geom_point() + 
+  geom_point(
+    mapping = aes(x =Price, y = Prod..year, data = filtered_data,
+                  colour = 'red',size = 3)
+  )
+
+par(mfrow =c(2,2))
+
+engine_mileage.lm <- lm(filtered_data$Engine.volume ~ filtered_data$Mileage,
+                        data = filtered_data)
+summary(engine_mileage.lm)
+par(mfrow = c(2,2))
+plot(engine_mileage.lm)
