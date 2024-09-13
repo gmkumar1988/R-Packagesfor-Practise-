e7ee702578d3d17cd9bd3f6d558b47d4b96549c4
Mahesh Gurumoorthi <gmkumar_1988@yahoo.com>
2024-09-13 15:37:22 -0400

    Linear Regression

--- Linear_Regression_Diamonds.R
+++ Linear_Regression_Diamonds.R
@@ -27,4 +27,27 @@ coef(fit)[1] + coef(fit)[2] * newx
 predict(fit, newdata = data.frame(carat = newx))
 predict(fit)
 
+###Residuals: 
+data(diamond)
+y <- diamond$price 
+x <- diamond$carat
+n <- length(y)
+fit <- lm(y ~ x)
+e <- resid(fit)
+yhat <- predict(fit)
+max(abs(e - (y - yhat)))
+max(abs(e - (y - coef(fit)[1] - coef(fit)[2] * x)))
+sum(e)
+sum(e * x)
+
+plot(x, e, 
+     xlab = "Mass(carots)",
+     ylab = "Residuals(SIN$)",
+     bg = 'lightblue',
+     col = 'black',cex = 2, pch = 21, frame = FALSE)
+abline(h= 0, lwd = 2)
+for(i in 1 : n)
+  lines(c(x[i],x[i]), c(e[i],0), col = 'red',lwd = 2)
+
+
 
