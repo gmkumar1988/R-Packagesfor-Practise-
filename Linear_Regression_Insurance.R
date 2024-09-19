8e8b8d0945ec057739f9062f1afd9328176c3376
Mahesh Gurumoorthi <gmkumar_1988@yahoo.com>
2024-09-19 01:23:17 -0400

    Linear Regression

---
+++ Linear_Regression_Insurance.R
@@ -0,0 +1,30 @@
+# ###Load the required packages: 
+# install.packages("ggplot2")
+# install.packages("dplyr")
+# install.packages("broom")
+# install.packages("ggpubr")
+
+library(ggplot2)
+library(dplyr)
+library(broom)
+library(ggpubr)
+
+
+insurance_data <- read.csv("/Users/maheshg/Dropbox/Sample Datasets Kaggle/insurance.csv")
+head(insurance_data)
+summary(insurance_data)
+
+
+hist(insurance_data$bmi)
+plot(bmi ~ age, data = insurance_data)
+cor(insurance_data$bmi, insurance_data$age)
+
+insurance_age_bmi <- lm(bmi ~ age, data = insurance_data)
+summary(insurance_age_bmi)
+
+par(mfrow = c(2,2))
+plot(insurance_age_bmi)
+
+insurance_graph <- ggplot(insurance_data, aes (x = bmi, y = age)) + 
+  geom_point()
+print(insurance_graph)
--- Multivariable_Regression.R
+++ Multivariable_Regression.R
@@ -10,3 +10,19 @@ ex = resid(lm(x ~ x2 + x3))
 coef(lm(ey ~ ex -1 ))
 sum(ey * ex) / sum(ex ^2)
 coef(lm(y ~ x + x2 + x3))
+
+
+###Multivariable regression 
+require(datasets)
+data("swiss")
+?swiss
+require(GGally)
+require(ggplot2)
+install.packages("GGally")
+library(GGally)
+# g = ggpairs(swiss, lower = list(continous = "smooth"),
+#             params = c(method = "loess"))
+
+summary(lm(Fertility ~ Agriculture, data = swiss))$coefficients
+
+help("wrap", package = "GGally")
