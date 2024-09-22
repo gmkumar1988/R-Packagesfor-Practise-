2dc366db7c44050a152246b5c154bf9e7393f33a
Mahesh Gurumoorthi <gmkumar_1988@yahoo.com>
2024-09-21 20:59:44 -0400

    Linear Regression Example2

--- LinearRegression_PetrolPrice.R
+++ LinearRegression_PetrolPrice.R
@@ -1,8 +1,8 @@
 # ###Load the required packages: 
-# install.packages("ggplot2")
-# install.packages("dplyr")
-# install.packages("broom")
-# install.packages("ggpubr")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("broom")
+install.packages("ggpubr")
 
 library(ggplot2)
 library(dplyr)
@@ -13,3 +13,22 @@ petrol <- read.csv("/Users/maheshg/Library/CloudStorage/OneDrive-Microsoft365/Sa
 head(petrol)
 cor(petrol)
 summary(petrol)
+
+hist(petrol$Petrol_Consumption)
+hist(petrol$Paved_Highways)
+
+plot(Petrol_Consumption ~ Paved_Highways, data = petrol)
+cor(petrol$Petrol_Consumption, petrol$Paved_Highways)
+
+###Perform linear regression line 
+petrol_consumption_highway <- lm(Petrol_Consumption ~ Paved_Highways, data = petrol)
+summary(petrol_consumption_highway)
+
+par(mfrow = c(2,4))
+
+plot(petrol_consumption_highway)
+
+petrol_highway_final <- ggplot(petrol_consumption_highway, aes(x = Petrol_Consumption,
+                                                               y = Paved_Highways )) + 
+  geom_point()
+print(petrol_highway_final)
